65197fdbd45f0779e116a599c69320896d2c851b
Maarten Somers <maarten.somers@trust1team.com>
2015-07-28 14:18:26 +0200

    Initial commit: Yeoman scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apimMarketApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the apimMarketApp
+ */
+angular.module('apimMarketApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name apimMarketApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the apimMarketApp
+ */
+angular.module('apimMarketApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
