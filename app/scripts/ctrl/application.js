f7598b64dd15923fd76817aafcc8bb5856b5afd3
Maarten Somers <maarten.somers@trust1team.com>
2015-09-07 16:24:40 +0200

    DPAPIM-126: Add button to break contract from application details screen

--- app/scripts/ctrl/application.js
+++ app/scripts/ctrl/application.js
@@ -6,56 +6,62 @@
 
 
 /// ==== Application Controller
-.controller("ApplicationCtrl", ["$scope", "$state", "$stateParams", "appData", "appVersions", "appScreenModel",
-  function ($scope, $state, $stateParams, appData, appVersions, appScreenModel) {
+    .controller("ApplicationCtrl", ["$scope", "$state", "$stateParams", "appData", "appVersions", "appScreenModel", "ApplicationContract",
+      function ($scope, $state, $stateParams, appData, appVersions, appScreenModel, ApplicationContract) {
 
-    $scope.applicationVersion = appData;
-    appScreenModel.updateApplication(appData);
-    $scope.versions = appVersions;
-    $scope.displayTab = appScreenModel;
-    $scope.isReady = $scope.applicationVersion.status === 'Ready';
-    $scope.isRegistered = $scope.applicationVersion.status === 'Registered' || $scope.applicationVersion.status === 'Retired';
-    $scope.isRetired = $scope.applicationVersion.status === 'Retired';
+        $scope.applicationVersion = appData;
+        appScreenModel.updateApplication(appData);
+        $scope.versions = appVersions;
+        $scope.displayTab = appScreenModel;
+        $scope.isReady = $scope.applicationVersion.status === 'Ready';
+        $scope.isRegistered = $scope.applicationVersion.status === 'Registered' || $scope.applicationVersion.status === 'Retired';
+        $scope.isRetired = $scope.applicationVersion.status === 'Retired';
 
-    $scope.selectVersion = function (version) {
-      $state.go($state.$current.name, { orgId: $stateParams.orgId, appId: $stateParams.appId, versionId: version.version});
-    };
+        $scope.selectVersion = function (version) {
+          $state.go($state.$current.name, { orgId: $stateParams.orgId, appId: $stateParams.appId, versionId: version.version});
+        };
 
-  }])
+        $scope.breakContract = function (contract) {
+          ApplicationContract.delete({orgId: contract.appOrganizationId, appId: contract.appId, versionId: contract.appVersion, contractId: contract.contractId}, function (reply) {
+            $state.forceReload();
+          });
+        };
 
-  // +++ Application Screen Subcontrollers +++
-  /// ==== Activity Controller
-  .controller("ActivityCtrl", ["$scope", "activityData", "appScreenModel", function ($scope, activityData, appScreenModel) {
+      }])
 
-    $scope.activities = activityData.beans;
-    appScreenModel.updateTab('Activity');
+    // +++ Application Screen Subcontrollers +++
+    /// ==== Activity Controller
+    .controller("ActivityCtrl", ["$scope", "activityData", "appScreenModel", function ($scope, activityData, appScreenModel) {
 
-  }])
-  /// ==== APIs Controller
-  .controller("ApisCtrl", ["$scope", "contractData", "appScreenModel", function ($scope, contractData, appScreenModel) {
+      $scope.activities = activityData.beans;
+      appScreenModel.updateTab('Activity');
 
-    $scope.contracts = contractData;
-    appScreenModel.updateTab('APIs');
+    }])
+    /// ==== APIs Controller
+    .controller("ApisCtrl", ["$scope", "contractData", "appScreenModel", function ($scope, contractData, appScreenModel) {
 
-    $scope.toggle = function () {
-      $scope.apiExpanded = !$scope.apiExpanded;
-    };
+      $scope.contracts = contractData;
+      appScreenModel.updateTab('APIs');
 
-  }])
-  /// ==== Contracts Controller
-  .controller("ContractsCtrl", ["$scope", "contractData", "appScreenModel", function ($scope, contractData, appScreenModel) {
+      $scope.toggle = function () {
+        $scope.apiExpanded = !$scope.apiExpanded;
+      };
 
-    $scope.contracts = contractData;
-    appScreenModel.updateTab('Contracts');
+    }])
+    /// ==== Contracts Controller
+    .controller("ContractsCtrl", ["$scope", "contractData", "appScreenModel", function ($scope, contractData, appScreenModel) {
 
-  }])
+      $scope.contracts = contractData;
+      appScreenModel.updateTab('Contracts');
 
-  /// ==== Overview Controller
-  .controller("OverviewCtrl", ["$scope", "appScreenModel", function ($scope, appScreenModel) {
+    }])
 
-    appScreenModel.updateTab('Overview');
+    /// ==== Overview Controller
+    .controller("OverviewCtrl", ["$scope", "appScreenModel", function ($scope, appScreenModel) {
 
-  }]);
+      appScreenModel.updateTab('Overview');
+
+    }]);
 
   // #end
 })();
