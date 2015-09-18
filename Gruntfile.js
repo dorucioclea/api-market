434168ffc61f864a54c9af1c31aafec72a20e755
Maarten Somers <maarten.somers@trust1team.com>
2015-09-18 14:43:54 +0200

    Include apimConfig.json in distribution builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -181,7 +181,8 @@ module.exports = function (grunt) {
             'fonts/**/*.*',
             'views/**/*.*',
             'scripts/plugins/*.js',
-            'styles/plugins/swagger/*.css'
+            'styles/plugins/swagger/*.css',
+            'apimConfig.json'
           ]
         }]
       }
