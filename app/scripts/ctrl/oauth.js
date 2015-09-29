71b45f04cd5580c56aacf8da7bb0b40176437361
Maarten Somers <maarten.somers@trust1team.com>
2015-09-29 16:36:27 +0200

    DPAPIM-209 Setup basic OAuth grant page, controller, styling

---
+++ app/scripts/ctrl/oauth.js
@@ -0,0 +1,11 @@
+;(function(angular) {
+    'use strict';
+
+    angular.module('app.ctrl.auth.oauth', [])
+
+        .controller('OAuthCtrl', ['$scope', function ($scope) {
+
+        }]);
+
+    // #end
+})(window.angular);
