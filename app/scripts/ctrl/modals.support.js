85e4f7c1dddf4ee0c5b884a47b49bdb93992b605
Maarten Somers <maarten.somers@trust1team.com>
2015-10-12 13:11:13 +0200

    DPAPIM-248 Service Owner can do any and all actions on the support page

--- app/scripts/ctrl/modals.support.js
+++ app/scripts/ctrl/modals.support.js
@@ -37,85 +37,90 @@
 
             }])
 
-    /// ==== ViewSupportTicket Controller
-    .controller('ViewSupportTicketCtrl', ['$scope', '$modal', '$state', 'currentUser', 'ticket', 'user',
+        /// ==== ViewSupportTicket Controller
+        .controller('ViewSupportTicketCtrl', ['$scope', '$modal', '$state', 'currentUser', 'ticket', 'user',
             'publisherMode', 'serviceVersion',
             'ServiceTicketComments', 'ServiceSupportTickets', 'toastService', 'TOAST_TYPES',
-        function ($scope, $modal, $state, currentUser, ticket, user, publisherMode, serviceVersion,
-                  ServiceTicketComments, ServiceSupportTickets, toastService, TOAST_TYPES) {
+            function ($scope, $modal, $state, currentUser, ticket, user, publisherMode, serviceVersion,
+                      ServiceTicketComments, ServiceSupportTickets, toastService, TOAST_TYPES) {
 
-            $scope.modalClose = modalClose;
-            $scope.comments = [];
-            $scope.comment = '';
-            $scope.currentUser = currentUser;
-            $scope.ticket = ticket;
-            $scope.user = user;
-            $scope.publisherMode = publisherMode;
-            $scope.serviceVersion = serviceVersion;
-            $scope.addComment = addComment;
-            $scope.deleteIssue = deleteIssue;
-            $scope.closeIssue = closeIssue;
-            $scope.reOpenIssue = reOpenIssue;
+                $scope.modalClose = modalClose;
+                $scope.comments = [];
+                $scope.comment = '';
+                $scope.currentUser = currentUser;
+                $scope.ticket = ticket;
+                $scope.user = user;
+                $scope.publisherMode = publisherMode;
+                $scope.serviceVersion = serviceVersion;
+                $scope.addComment = addComment;
+                $scope.deleteIssue = deleteIssue;
+                $scope.closeIssue = closeIssue;
+                $scope.reOpenIssue = reOpenIssue;
+                $scope.isServiceOwner = isServiceOwner;
 
-            ServiceTicketComments.query({supportId: $scope.ticket.id}, function (reply) {
-                $scope.comments = reply;
-                $scope.comments.sort(function (a, b) {
-                    return a.createdOn - b.createdOn;
+                ServiceTicketComments.query({supportId: $scope.ticket.id}, function (reply) {
+                    $scope.comments = reply;
+                    $scope.comments.sort(function (a, b) {
+                        return a.createdOn - b.createdOn;
+                    });
                 });
-            });
 
-            function addComment() {
-                var commentObject = {
-                    comment: $scope.comment
-                };
-                ServiceTicketComments.save({supportId: $scope.ticket.id}, commentObject, function (reply) {
-                    $scope.comment = '';
-                    $scope.commentForm.$setUntouched();
-                    $scope.comments.push(reply);
-                    toastService.createToast(TOAST_TYPES.INFO, '<b>Comment posted.</b>', true);
-                });
-            }
+                function addComment() {
+                    var commentObject = {
+                        comment: $scope.comment
+                    };
+                    ServiceTicketComments.save({supportId: $scope.ticket.id}, commentObject, function (reply) {
+                        $scope.comment = '';
+                        $scope.commentForm.$setUntouched();
+                        $scope.comments.push(reply);
+                        toastService.createToast(TOAST_TYPES.INFO, '<b>Comment posted.</b>', true);
+                    });
+                }
 
-            function deleteIssue() {
-                ServiceSupportTickets.remove({orgId: $scope.ticket.organizationId,
-                    svcId: $scope.ticket.serviceId, supportId: $scope.ticket.id}, function (reply) {
-                    $scope.modalClose();
-                    toastService.createToast(TOAST_TYPES.WARNING, '<b>Ticket deleted.</b>', true);
-                });
-            }
+                function deleteIssue() {
+                    ServiceSupportTickets.remove({orgId: $scope.ticket.organizationId,
+                        svcId: $scope.ticket.serviceId, supportId: $scope.ticket.id}, function (reply) {
+                        $scope.modalClose();
+                        toastService.createToast(TOAST_TYPES.WARNING, '<b>Ticket deleted.</b>', true);
+                    });
+                }
 
-            function closeIssue() {
-                var ticketObject = {
-                    title: $scope.ticket.title,
-                    description: $scope.ticket.description,
-                    status: 'CLOSED'
-                };
-                ServiceSupportTickets.update({orgId: $scope.ticket.organizationId,
-                    svcId: $scope.ticket.serviceId, supportId: $scope.ticket.id}, ticketObject, function (reply) {
-                    $scope.modalClose();
-                    toastService.createToast(TOAST_TYPES.INFO, '<b>Ticket closed.</b>', true);
-                });
-            }
+                function closeIssue() {
+                    var ticketObject = {
+                        title: $scope.ticket.title,
+                        description: $scope.ticket.description,
+                        status: 'CLOSED'
+                    };
+                    ServiceSupportTickets.update({orgId: $scope.ticket.organizationId,
+                        svcId: $scope.ticket.serviceId, supportId: $scope.ticket.id}, ticketObject, function (reply) {
+                        $scope.modalClose();
+                        toastService.createToast(TOAST_TYPES.INFO, '<b>Ticket closed.</b>', true);
+                    });
+                }
 
-            function reOpenIssue() {
-                var ticketObject = {
-                    title: $scope.ticket.title,
-                    description: $scope.ticket.description,
-                    status: 'OPEN'
-                };
-                ServiceSupportTickets.update({orgId: $scope.ticket.organizationId,
-                    svcId: $scope.ticket.serviceId, supportId: $scope.ticket.id}, ticketObject, function (reply) {
-                    $scope.modalClose();
-                    toastService.createToast(TOAST_TYPES.INFO, '<b>Ticket reopened!</b>', true);
-                });
-            }
+                function reOpenIssue() {
+                    var ticketObject = {
+                        title: $scope.ticket.title,
+                        description: $scope.ticket.description,
+                        status: 'OPEN'
+                    };
+                    ServiceSupportTickets.update({orgId: $scope.ticket.organizationId,
+                        svcId: $scope.ticket.serviceId, supportId: $scope.ticket.id}, ticketObject, function (reply) {
+                        $scope.modalClose();
+                        toastService.createToast(TOAST_TYPES.INFO, '<b>Ticket reopened!</b>', true);
+                    });
+                }
 
-            function modalClose() {
-                $scope.$close();	// this method is associated with $modal scope which is this.
-                $state.forceReload();
-            }
+                function modalClose() {
+                    $scope.$close();	// this method is associated with $modal scope which is this.
+                    $state.forceReload();
+                }
 
-        }]);
+                function isServiceOwner() {
+                    return $scope.serviceVersion.createdBy === $scope.currentUser.currentUser.username;
+                }
+
+            }]);
 
     // #end
 })(window.angular);
