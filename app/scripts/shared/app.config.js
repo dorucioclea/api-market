918dfe58c7f622a2b675ec8a6cc11e15a5b4e2cd
Michallis Pashidis <michallis@trust1tream.com>
2015-12-04 21:38:45 +0100

    update config

--- app/scripts/shared/app.config.js
+++ app/scripts/shared/app.config.js
@@ -1,30 +1,10 @@
-;(function(angular) {
-    'use strict';
+;(function(angular){
+"use strict";
 
-    angular.module('app.config', [])
-        .constant('CONFIG', {
-            BASE: {
-                URL:'https://dev.apim.t1t.be:8443/dev/apiengine/v1',
-                JWT_HEADER_NAME: 'jwt'
-            },
-            AUTH: {
-                URL: 'https://dev.apim.t1t.be:8443/dev/apiengineauth/v1'
-            },
-            STORAGE: {
-                LOCAL_STORAGE: 'apim-',
-                SESSION_STORAGE: 'apim_session-'
-            },
-            SECURITY: {
-                REDIRECT_URL: '/login/idp/redirect',
-                API_KEY: '6b8406cc81fe4ca3cc9cd4a0abfb97c2',
-                IDP_URL: 'https://dev.idp.t1t.be:9443/samlsso',
-                SP_URL: 'http://dev.api.t1t.be/API-Engine-auth/v1/login/idp/callback',
-                SP_NAME: 'apimmarket',
-                CLIENT_TOKEN: 'jwt'
-            },
-            KONG: {
-                HOST: 'dev.apim.t1t.be:8443'
-            }
-        });
+angular.module('app.config', [])
+
+.constant('CONFIG', {BASE:{URL:'https://devapim.t1t.be/dev/apiengine/v1',JWT_HEADER_NAME:'jwt'},AUTH:{URL:'https://devapim.t1t.be/dev/apiengineauth/v1'},STORAGE:{LOCAL_STORAGE:'apim-',SESSION_STORAGE:'apim_session-'},SECURITY:{REDIRECT_URL:'/login/idp/redirect',API_KEY:'6b8406cc81fe4ca3cc9cd4a0abfb97c2',IDP_URL:'https://devidp.t1t.be/samlsso',SP_URL:'http://devapi.t1t.be/API-Engine-auth/v1/login/idp/callback',SP_NAME:'apimmarket',CLIENT_TOKEN:'jwt'},KONG:{HOST:'devapim.t1t.be'}})
+
+; 
 
 })(window.angular);
