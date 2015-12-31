4781e70883ce53d3a86667c10d9376a71054f933
Michallis Pashidis <michallis@trust1team.com>
2015-12-31 10:30:16 +0100

    update grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -485,66 +485,64 @@ module.exports = function (grunt) {
             }
           }
         }
-      }
-    },
-        digiProdPub: {
-          constants: {
-            'CONFIG': {
-              'BASE': {
-                'URL': 'https://api-gw-p.antwerpen.be/apiengine/v1',
-                'JWT_HEADER_NAME': 'jwt'
-              },
-              'AUTH': {
-                'URL': 'https://api-gw-p.antwerpen.be/apiengineauth/v1'
-              },
-              'STORAGE': {
-                'LOCAL_STORAGE': 'apim-',
-                'SESSION_STORAGE': 'apim_session-'
-              },
-              'SECURITY': {
-                'REDIRECT_URL': '/login/idp/redirect',
-                'API_KEY': '05bac13c95a346cbc6e177d747e038db',
-                'IDP_URL': 'https://identityserver.antwerpen.be/samlsso',
-                'SP_URL': 'https://api-engine-p.antwerpen.be/API-Engine-auth/v1/login/idp/callback',
-                'SP_NAME': 'apiengine',
-                'CLIENT_TOKEN': 'jwt'
-              },
-              KONG: {
-                HOST: 'api-gw-p.antwerpen.be'
-              }
-            }
-          }
-        },
-        digiProdMkt: {
-          constants: {
-            'CONFIG': {
-              'BASE': {
-                'URL': 'https://api-gw-p.antwerpen.be/apiengine/v1',
-                'JWT_HEADER_NAME': 'jwt'
-              },
-              'AUTH': {
-                'URL': 'https://api-gw-p.antwerpen.be/apiengineauth/v1'
-              },
-              'STORAGE': {
-                'LOCAL_STORAGE': 'apim-',
-                'SESSION_STORAGE': 'apim_session-'
-              },
-              'SECURITY': {
-                'REDIRECT_URL': '/login/idp/redirect',
-                'API_KEY': '229e2ea08ba94919c9d221cdf3be1f7d',
-                'IDP_URL': 'https://identityserver.antwerpen.be/samlsso',
-                'SP_URL': 'https://api-engine-p.antwerpen.be/API-Engine-auth/v1/login/idp/callback',
-                'SP_NAME': 'apiengine',
-                'CLIENT_TOKEN': 'jwt'
-              },
-              KONG: {
-                HOST: 'api-gw-p.antwerpen.be'
-              }
+      },
+      digiProdPub: {
+        constants: {
+          'CONFIG': {
+            'BASE': {
+              'URL': 'https://api-gw-p.antwerpen.be/apiengine/v1',
+              'JWT_HEADER_NAME': 'jwt'
+            },
+            'AUTH': {
+              'URL': 'https://api-gw-p.antwerpen.be/apiengineauth/v1'
+            },
+            'STORAGE': {
+              'LOCAL_STORAGE': 'apim-',
+              'SESSION_STORAGE': 'apim_session-'
+            },
+            'SECURITY': {
+              'REDIRECT_URL': '/login/idp/redirect',
+              'API_KEY': '05bac13c95a346cbc6e177d747e038db',
+              'IDP_URL': 'https://identityserver.antwerpen.be/samlsso',
+              'SP_URL': 'https://api-engine-p.antwerpen.be/API-Engine-auth/v1/login/idp/callback',
+              'SP_NAME': 'apiengine',
+              'CLIENT_TOKEN': 'jwt'
+            },
+            KONG: {
+              HOST: 'api-gw-p.antwerpen.be'
             }
           }
         }
       },
-
+      digiProdMkt: {
+        constants: {
+          'CONFIG': {
+            'BASE': {
+              'URL': 'https://api-gw-p.antwerpen.be/apiengine/v1',
+              'JWT_HEADER_NAME': 'jwt'
+            },
+            'AUTH': {
+              'URL': 'https://api-gw-p.antwerpen.be/apiengineauth/v1'
+            },
+            'STORAGE': {
+              'LOCAL_STORAGE': 'apim-',
+              'SESSION_STORAGE': 'apim_session-'
+            },
+            'SECURITY': {
+              'REDIRECT_URL': '/login/idp/redirect',
+              'API_KEY': '229e2ea08ba94919c9d221cdf3be1f7d',
+              'IDP_URL': 'https://identityserver.antwerpen.be/samlsso',
+              'SP_URL': 'https://api-engine-p.antwerpen.be/API-Engine-auth/v1/login/idp/callback',
+              'SP_NAME': 'apiengine',
+              'CLIENT_TOKEN': 'jwt'
+            },
+            KONG: {
+              HOST: 'api-gw-p.antwerpen.be'
+            }
+          }
+        }
+      }
+    },
     // ===== //
     // Karma //
     // ===== //
