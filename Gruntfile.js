954e10e252b47652dce7dcc84cb4bc9c6e4dda4d
Maarten Somers <maarten.somers@gmail.com>
2016-07-05 10:14:50 +0200

    Updated SP Names for digi-dev

--- Gruntfile.js
+++ Gruntfile.js
@@ -650,7 +650,7 @@ module.exports = function (grunt) {
               'API_KEY': '05bac13c95a346cbc6e177d747e038db',
               'IDP_URL': 'https://identityserver-o.antwerpen.be/samlsso',
               'SP_URL': 'https://api-engine-o.antwerpen.be/API-Engine-auth/v1/login/idp/callback',
-              'SP_NAME': 'apiengine',
+              'SP_NAME': 'apiengine-o',
               'CLIENT_TOKEN': 'jwt',
               'WSO2_LOGIN_FIX': true
             },
@@ -687,7 +687,7 @@ module.exports = function (grunt) {
               'API_KEY': '229e2ea08ba94919c9d221cdf3be1f72',
               'IDP_URL': 'https://identityserver-o.antwerpen.be/samlsso',
               'SP_URL': 'https://api-engine-o.antwerpen.be/API-Engine-auth/v1/login/idp/callback',
-              'SP_NAME': 'apiengine',
+              'SP_NAME': 'apiengine-o',
               'CLIENT_TOKEN': 'jwt',
               'WSO2_LOGIN_FIX': true
             },
@@ -724,7 +724,7 @@ module.exports = function (grunt) {
               'API_KEY': '229e2ea08ba94919c9d221cdf3be1f71',
               'IDP_URL': 'https://identityserver-o.antwerpen.be/samlsso',
               'SP_URL': 'https://api-engine-o.antwerpen.be/API-Engine-auth/v1/login/idp/callback',
-              'SP_NAME': 'apiengine',
+              'SP_NAME': 'apiengine-o',
               'CLIENT_TOKEN': 'jwt',
               'WSO2_LOGIN_FIX': true
             },
