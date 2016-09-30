42cd871396dde87305a17c0b91a3068be8ec5e57
Maarten Somers <maarten.somers@gmail.com>
2016-09-30 15:56:49 +0200

    Show warning toast instead of info toast when removing a policy

--- app/scripts/policies/policies.component.js
+++ app/scripts/policies/policies.component.js
@@ -82,14 +82,14 @@
                         case 'plan':
                             policyService.deletePlanPolicy($stateParams.orgId, $stateParams.planId, $stateParams.versionId, policy.id).then(
                                 function () {
-                                    toastService.info('' + policy.name + ' removed.');
+                                    toastService.warning('' + policy.name + ' removed.');
                                     _.remove(controller.policies, policy);
                                 });
                             break;
                         case 'svc':
                             policyService.deleteServicePolicy($stateParams.orgId, $stateParams.svcId, $stateParams.versionId, policy.id).then(
                                 function () {
-                                    toastService.info('' + policy.name + ' removed.');
+                                    toastService.warning('' + policy.name + ' removed.');
                                     _.remove(controller.policies, policy);
                                 });
                             break;
