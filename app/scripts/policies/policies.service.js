f8e102498395e7dfe2d4d528866ceb689044c859
Guillaume Vandecasteele <guillaume@trust1team.com>
2016-11-15 12:27:48 +0100

    Fix for admin services swagger tryout not including JWT

--- app/scripts/policies/policies.service.js
+++ app/scripts/policies/policies.service.js
@@ -41,6 +41,7 @@
 
         function generateDetailsPopover(definitionId, configuration) {
             var parsedConfiguration = angular.fromJson(configuration);
+            console.log(configuration);
             switch (definitionId) {
                 case POLICIES.ACL:
                     return generateAclPopover(parsedConfiguration);
@@ -307,6 +308,7 @@
 
             function generateLDAPPopover(config) {
                 if (CONFIG.APP.PUBLISHER_MODE) {
+                    console.log(config);
                     var string = '';
                     string += '<p class="text-light">LDAP host: <b>' + config.ldap_host + ':' + config.ldap_port + '</b></p>';
                     string += '<ul class="text-light">';
@@ -393,6 +395,8 @@
                 _.forEach(policies, function (policy) {
                     promises.push(getServicePolicyDetails(orgId, svcId, versionId, policy.id).then(function (details) {
                         policy.details = details;
+                        console.log(policy);
+                        console.log(details);
                         policy.popover = generateDetailsPopover(policy.policyDefinitionId, details.configuration);
                         policy.description = getPolicyDescription(policy.policyDefinitionId);
                         policy.iconPath = getPolicyIcon(policy.policyDefinitionId);
